7ed860946dfb51716040123296d7e03cb52a9418
Andrei <andreigabor36@gmail.com>
2022-02-22 16:07:29 +0200

    First lab done in classroom

---
+++ TeacherMonitor/src/main/java/ssvv/monitor/app/MainApplication.java
@@ -0,0 +1,37 @@
+package ssvv.monitor.app;
+
+
+import ssvv.monitor.repository.StudentXMLRepo;
+import ssvv.monitor.repository.TemaXMLRepo;
+import ssvv.monitor.repository.NotaXMLRepo;
+import ssvv.monitor.service.Service;
+import ssvv.monitor.view.UI;
+import ssvv.monitor.validation.NotaValidator;
+import ssvv.monitor.validation.StudentValidator;
+import ssvv.monitor.validation.TemaValidator;
+
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+       //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        UI ui = new UI(service);
+        ui.run();
+    }
+
+}
---
+++ TeacherMonitor/src/main/java/ssvv/monitor/validation/NotaValidator.java
@@ -0,0 +1,44 @@
+package ssvv.monitor.validation;
+
+
+import ssvv.monitor.domain.Nota;
+import ssvv.monitor.domain.Student;
+import ssvv.monitor.domain.Tema;
+import ssvv.monitor.repository.StudentXMLRepo;
+import ssvv.monitor.repository.TemaXMLRepo;
+
+public class NotaValidator implements Validator<Nota> {
+    private StudentXMLRepo studentFileRepository;
+    private TemaXMLRepo temaFileRepository;
+
+    /**
+     * Class constructor
+     * @param studentFileRepository - repository student
+     * @param temaFileRepository - repository tema
+     */
+    public NotaValidator(StudentXMLRepo studentFileRepository, TemaXMLRepo temaFileRepository) {
+        this.studentFileRepository = studentFileRepository;
+        this.temaFileRepository = temaFileRepository;
+    }
+
+    /**
+     * Valideaza o nota
+     * @param nota - nota pe care o valideaza
+     * @throws ValidationException daca nota nu e valida
+     */
+    @Override
+    public void validate(Nota nota) throws ValidationException {
+        Student student = studentFileRepository.findOne(nota.getIdStudent());
+        if (student== null){
+            throw new ValidationException("Studentul nu exista!");
+        }
+        Tema tema = temaFileRepository.findOne(nota.getIdTema());
+        if(tema == null){
+            throw new ValidationException("Tema nu exista!");
+        }
+        double notaC = nota.getNota();
+        if(notaC > 10.00 || notaC < 0.00){
+            throw new ValidationException("Valoarea notei nu este corecta!");
+        }
+    }
+}
