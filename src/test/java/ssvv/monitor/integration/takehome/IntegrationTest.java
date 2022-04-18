cb1b24df205e0ae23088eb72c937c49a8e1320b6
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2022-04-19 02:17:24 +0300

    renamed test class

--- src/test/java/ssvv/monitor/integration/takehome/IntegrationTest.java
+++ src/test/java/ssvv/monitor/integration/takehome/IntegrationTest.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class Integration {
+public class IntegrationTest {
 
     private Service service;
 
